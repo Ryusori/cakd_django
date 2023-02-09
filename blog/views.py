66c0608b64c8b9b7b37a2e70c6772d2b09c4404a
unknown <sory02191@gmail.com>
2023-02-09 10:45:29 +0900

    templates 모듈화

--- blog/views.py
+++ blog/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Post
 
-class Postlist(ListView):
+class Postlist(ListView): #class방식으로 처리했다.
     model = Post
-    ordering = '-pk'
+    ordering = '-pk'   # 최근에 온게 가장 먼저띄겠끔
 
 
 class PostDetail(DetailView):
-    model = Post
+    model = Post       
 
 
 
